bc06c3495a862d4d4dbd71b9c3c516382863d204
Vincent Mahnke <vincent@mahn.ke>
2025-11-29 23:14:26 +0100

    chore: Downgrades provider

--- infrastructure/autogenerated-main.tf
+++ infrastructure/autogenerated-main.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     allinkl = {
       source  = "ViMaSter/allinkl"
-      version = "0.1.7"
+      version = "0.1.4"
     }
     keycloak = {
       source  = "keycloak/keycloak"
